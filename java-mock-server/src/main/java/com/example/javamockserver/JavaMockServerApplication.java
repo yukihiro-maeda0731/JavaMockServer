389cad69c3f0e3bad141cdcd7df9942107e576e6
前田 <sloth19920731@gmail.com>
2022-02-03 23:28:48 +0900

    pj作成

---
+++ java-mock-server/src/main/java/com/example/javamockserver/JavaMockServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.javamockserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMockServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMockServerApplication.class, args);
+	}
+
+}
---
+++ java-mock-server/src/test/java/com/example/javamockserver/JavaMockServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javamockserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMockServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
